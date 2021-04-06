8f48d56a64c28c92749f20aef94e9054e439c3bc
Bojke <danijelbojcic@gmail.com>
2021-04-06 14:59:54 +0200

    added typescript support

---
+++ lib/typescript/PreviewVideoView.d.ts
@@ -0,0 +1,5 @@
+declare const View: import("react-native").HostComponent<{
+    deviceId: number;
+    objectFit: "contain" | "cover";
+}>;
+export default View;
---
+++ lib/typescript/RemoteVideoView.d.ts
@@ -0,0 +1,5 @@
+declare const View: import("react-native").HostComponent<{
+    windowId: string;
+    objectFit: "contain" | "cover";
+}>;
+export default View;
